613acd0782b0f7785d936d48839ab9850683c5cf
Azeem Sajjad <azeemsajjad@Azeems-MacBook-Pro.local>
2023-08-06 09:36:53 +0530

    ok

--- routes/train.js
+++ routes/train.js
@@ -5,11 +5,27 @@ const {
   getTrainDetails,
   getTrainCoach,
   coachComposition,
+  getStationSuggestion,
+  getTrainsBetweenStation,
+  searchTrain,
+  getRunningStatus,
+  getPNR,
 } = require("../controller/train");
 
-router.route("/").get(getAllTrains);
-router.route("/get-details").post(getTrainDetails);
-router.route("/get-coach").post(getTrainCoach);
-router.route("/get-coach-composition").post(coachComposition);
+const authMiddleware = require("../middleware/auth");
+
+router.route("/").get(authMiddleware, getAllTrains);
+router.route("/get-details").post(authMiddleware, getTrainDetails);
+router.route("/get-coach").post(authMiddleware, getTrainCoach);
+router.route("/get-coach-composition").post(authMiddleware, coachComposition);
+router
+  .route("/get-station-suggestion/:query")
+  .get(authMiddleware, getStationSuggestion);
+router
+  .route("/get-trains-between")
+  .post(authMiddleware, getTrainsBetweenStation);
+router.route("/search-train/:query").get(authMiddleware, searchTrain);
+router.route("/get-running-status").post(authMiddleware, getRunningStatus);
+router.route("/get-pnr-status/:pnr").get(authMiddleware, getPNR);
 
 module.exports = router;
