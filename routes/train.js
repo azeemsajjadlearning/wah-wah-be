136846431b663788d06e4a128e2d1496211452a2
Azeem Sajjad <azeemsajjad@Azeems-MacBook-Pro.local>
2023-08-15 14:43:55 +0530

    ok

--- routes/train.js
+++ routes/train.js
@@ -9,9 +9,9 @@ const {
 
 const authMiddleware = require("../middleware/auth");
 
-router.route("/get-all-stations").get(authMiddleware, getAllStations);
-router.route("/get-all-trains").get(authMiddleware, getAllTrains);
-router.route("/get-bw-trains").post(authMiddleware, getTrains);
-router.route("/get-running-status").post(authMiddleware, getRunningStatus);
+router.route("/get-all-stations").get(getAllStations);
+router.route("/get-all-trains").get(getAllTrains);
+router.route("/get-bw-trains").post(getTrains);
+router.route("/get-running-status").post(getRunningStatus);
 
 module.exports = router;
