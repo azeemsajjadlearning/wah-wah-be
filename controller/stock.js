4b0b8730ccf259b99f8f5d67088ddcd4dca76cc2
Azeem Sajjad <azeemsajjad@Azeems-MacBook-Pro.local>
2023-08-28 09:59:24 +0530

    ok

--- controller/stock.js
+++ controller/stock.js
@@ -1,145 +1,142 @@
 var axios = require("axios");
-const request = require("request");
 const { StatusCodes } = require("http-status-codes");
-const mongoose = require("mongoose");
-const MutualFund = require("../models/MutualFund");
 
-const uploadMFData = (req, res) => {
-  var options = {
-    method: "GET",
-    url: "https://latest-mutual-fund-nav.p.rapidapi.com/fetchLatestNAV",
-    headers: {
-      "X-RapidAPI-Host": process.env.RAPIDAPI_MF_HOST,
-      "X-RapidAPI-Key": process.env.RAPIDAPI_MF_KEY,
-    },
-  };
-  request(options, async function (error, response) {
-    if (error) {
-      res.send({ success: false, error });
-      throw new Error(error);
-    }
-
-    let obj = {};
-
-    Object.keys(JSON.parse(response.body)[0]).forEach((ele) => {
-      if (ele !== "Date")
-        obj[ele.toString().toLowerCase().replace(/ /g, "_")] = "String";
-      else obj[ele.toString().toLowerCase().replace(/ /g, "_")] = "Date";
-    });
-
-    const newSample = JSON.parse(response.body).map((obj) => {
-      const newObj = {};
-      Object.keys(obj).forEach((key) => {
-        obj["Date"] = new Date(obj["Date"]);
-        newObj[key.toLowerCase().replace(/ /g, "_")] = obj[key];
-      });
-      return newObj;
-    });
-
-    const responseSchema = new mongoose.Schema(obj, { timestamps: true });
-    const MutualFund = mongoose.model("MutualFund", responseSchema);
-
-    await MutualFund.deleteMany({});
-
-    try {
-      const upload = await MutualFund.insertMany(newSample);
-      res
-        .status(StatusCodes.CREATED)
-        .send({ success: true, result: upload.length + " uploaded" });
-    } catch (error) {
-      console.log(error);
-      res.send({ success: false, err: error });
-    }
-  });
-};
-
-const getMFData = async (req, res) => {
+const getPopularMF = async (req, res) => {
   try {
-    const { page, q } = req.query;
-    const mf = await MutualFund.find({
-      scheme_name: { $regex: new RegExp(q, "i") },
-    })
-      .sort({ date: -1 })
-      .skip(page * 100)
-      .limit(100);
-    res.status(StatusCodes.OK).send({ success: true, result: mf });
+    const resp = await axios.get(
+      "https://groww.in/v1/api/data/mf/web/v1/collection",
+      {
+        params: {
+          actTime: new Date().getTime(),
+          cid: "popular_direct_mf",
+          doc_required: false,
+        },
+      }
+    );
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
+    });
   } catch (error) {
     console.log(error);
-    res.send({ success: false, err: error });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
   }
 };
 
-const getLatestNAV = async (req, res) => {
+const searchMF = async (req, res) => {
   try {
-    axios
-      .get(
-        "https://latest-mutual-fund-nav.p.rapidapi.com/fetchLatestNAV?SchemeCode=" +
-          req.params.scheme_code,
-        {
-          headers: {
-            "X-RapidAPI-Host": process.env.RAPIDAPI_MF_HOST,
-            "X-RapidAPI-Key": process.env.RAPIDAPI_MF_KEY,
-          },
-        }
-      )
-      .then((resp) => {
-        res.status(StatusCodes.OK).send({ success: true, result: resp.data });
-      })
-      .catch((err) => {
-        console.log(error);
-        res.send({ success: false, err: err });
-      });
-  } catch (error) {
-    console.log(error);
-    res.send({ success: false, err: error });
-  }
-};
-
-const getNavHistory = async (req, res) => {
-  try {
-    const { start_date, end_date } = req.query;
-    let dates = getDates(new Date(start_date), new Date(end_date));
-    var options = {
-      method: "GET",
-      url:
-        "https://latest-mutual-fund-nav.p.rapidapi.com/fetchHistoricalNAV?Date=" +
-        dates +
-        "&SchemeCode=" +
-        req.params.scheme_code,
-      headers: {
-        "X-RapidAPI-Host": process.env.RAPIDAPI_MF_HOST,
-        "X-RapidAPI-Key": process.env.RAPIDAPI_MF_KEY,
+    const resp = await axios.get("https://groww.in/v1/api/search/v1/entity", {
+      params: {
+        app: false,
+        page: req.body.page || 0,
+        q: req.body.query,
+        size: 20,
       },
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      res
-        .status(StatusCodes.OK)
-        .send({ success: true, result: JSON.parse(response.body) });
+    });
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
     });
   } catch (error) {
     console.log(error);
-    res.send({ success: false, err: error });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
   }
 };
 
-function getDates(startDate, endDate) {
-  var dates = [],
-    currentDate = startDate,
-    addDays = function (days) {
-      var date = new Date(this.valueOf());
-      date.setDate(date.getDate() + days);
-      return date;
-    };
-  while (currentDate <= endDate) {
-    var date = currentDate.getDate().toString().padStart(2, "0");
-    var month = currentDate.toLocaleString("default", { month: "short" });
-    var year = currentDate.getFullYear();
-    var dateString = `${date}-${month}-${year}`;
-    dates.push(dateString);
-    currentDate = addDays.call(currentDate, 1);
+const getCollections = async (req, res) => {
+  try {
+    const resp = await axios.get(
+      "https://groww.in/v1/api/data/mf/web/v1/collections/" + req.params.type
+    );
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
   }
-  return dates.join(", ");
-}
 
-module.exports = { uploadMFData, getMFData, getLatestNAV, getNavHistory };
+  // type =
+  //   "best-high-return" |
+  //   "best-sip-with-500" |
+  //   "best-tax-saving" |
+  //   "best-large-cap" |
+  //   "best-mid-cap" |
+  //   "best-small-cap";
+};
+
+const getMFInfo = async (req, res) => {
+  try {
+    const resp = await axios.get(
+      "https://groww.in/v1/api/data/mf/web/v2/scheme/search/" +
+        req.params.search_id,
+      { params: { include_swp_frequency: false } }
+    );
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
+  }
+};
+
+const getMFGraph = async (req, res) => {
+  try {
+    const resp = await axios.get(
+      "https://groww.in/v1/api/data/mf/web/v1/scheme/" +
+        req.body.scheme_code +
+        "/graph",
+      { params: { benchmark: false, months: req.body.months || 36 } }
+    );
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
+  }
+};
+
+const getMFDetails = async (req, res) => {
+  try {
+    const resp = await axios.post(
+      "https://groww.in/v1/api/bse/v1/scheme/details",
+      {
+        isin: req.body.isin,
+        schemeType: req.body.scheme_type,
+      }
+    );
+    res.status(StatusCodes.OK).send({
+      success: true,
+      result: resp.data,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ success: false, err: error });
+  }
+};
+
+module.exports = {
+  getPopularMF,
+  searchMF,
+  getCollections,
+  getMFInfo,
+  getMFGraph,
+  getMFDetails,
+};
--- routes/stock.js
+++ routes/stock.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  uploadMFData,
-  getMFData,
-  getLatestNAV,
-  getNavHistory,
+  getPopularMF,
+  searchMF,
+  getCollections,
+  getMFInfo,
+  getMFGraph,
+  getMFDetails,
 } = require("../controller/stock");
 
 const authMiddleware = require("../middleware/auth");
 
-router.route("/upload-mf").get(authMiddleware, uploadMFData);
-router.route("/get-mf").get(authMiddleware, getMFData);
-router.route("/get-mf-latest/:scheme_code").get(authMiddleware, getLatestNAV);
-router.route("/get-mf-history/:scheme_code").get(authMiddleware, getNavHistory);
+router.route("/get-popular-mf").get(authMiddleware, getPopularMF);
+router.route("/search-mf").post(authMiddleware, searchMF);
+router.route("/get-collections/:type").get(authMiddleware, getCollections);
+router.route("/get-mf-info/:search_id").get(authMiddleware, getMFInfo);
+router.route("/get-mf-graph").post(authMiddleware, getMFGraph);
+router.route("/get-mf-details").post(authMiddleware, getMFDetails);
 
 module.exports = router;
